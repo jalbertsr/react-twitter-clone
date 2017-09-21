56a07dc74fd550304109793bc00a191b524ac7cf
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-09-21 20:45:29 +0200

    config firebase done

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import firebase from 'firebase'
 import { render } from 'react-dom'
 
 import App from './components/app'
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyA3RhCuAvwYlah5PJNSIHqMGmbl70v5rHc',
+  authDomain: 'github-twitter-41292.firebaseapp.com',
+  databaseURL: 'https://github-twitter-41292.firebaseio.com',
+  projectId: 'github-twitter-41292',
+  storageBucket: 'github-twitter-41292.appspot.com',
+  messagingSenderId: '272904097178'
+})
+
 render(<App />, document.getElementById('root'))
+
