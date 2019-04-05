f77fb08807df3a0b5c3d57fb88276323b45b2a56
Joan Albert Segura <jalbertsr@protonmail.ch>
2019-04-05 15:56:19 +0200

    use new proptypes

--- src/components/message/index.jsx
+++ src/components/message/index.jsx
@@ -1,5 +1,6 @@
-import React, { Component, PropTypes } from 'react'
-import { Link } from 'react-router'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import styles from './styles.css'
 import moment from 'moment'
 
